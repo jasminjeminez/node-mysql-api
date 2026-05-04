066035c10bfe6fb02a4758a0256add14372aa297
Jasmin Jeminez <jasminjeminez12@gmail.com>
2026-05-04 21:05:18 +0800

    use env variables for db connection

--- node-mysql-api/_helpers/db.ts
+++ node-mysql-api/_helpers/db.ts
@@ -1,4 +1,3 @@
-import config from '../config.json';
 import mysql from 'mysql2/promise';
 import { Sequelize } from 'sequelize';
 import accountModel from '../accounts/account.model';
@@ -10,23 +9,27 @@ export default db;
 initialize();
 
 async function initialize() {
-    const { host, port, user, password, database } = config.database;
+    const host = process.env.DB_HOST!;
+    const port = parseInt(process.env.DB_PORT || '3306');
+    const user = process.env.DB_USER!;
+    const password = process.env.DB_PASSWORD!;
+    const database = process.env.DB_NAME!;
+
     const connection = await mysql.createConnection({ host, port, user, password });
 
-    // Create DB if it doesn't exist
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-    // Connect to DB
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, {
+        host,
+        port,
+        dialect: 'mysql'
+    });
 
-    // Init models
     db.Account = accountModel(sequelize);
     db.RefreshToken = refreshTokenModel(sequelize);
 
-    // Define relationships
     db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
 
-    // Sync models with database
     await sequelize.sync();
 }
